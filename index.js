8a8a351bf08a9fb084c50238a318a038a346e23a
meharasbeham <128901764+meharasbeham@users.noreply.github.com>
2023-12-22 01:12:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-// 
-const http=require('http')
+const express=require('express')
+const app=express()
 const PORT=5000
-let server=new http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'})
-    res.end("welcome meha")
-})
+app.use(express.json())
 
-server.listen(PORT,()=>console.log("serverwait"+PORT))
+const data=[{
+    customarname:"raja",
+    date:"1-1-24",
+    starttime:"10am",
+    endtime:"9pm",
+    roomid:"1",
+}]
+ app.get('/:id',(req,res)=>{
+    try{
+        let id=Number (req.params.id)
+        if(id<data.length)
+    {
+        res.status(200).send({
+        data:data[id]
+        })
+    }else{
+        req.status(400).send({
+            message:"invalid id"
+        })
+    }
+}catch (error){
+    res.status(500).send({
+        message:"internal server error",
+        error:error.message
+    })
+}
+})
+ app.listen(PORT,()=>console.log(`App Listening ${PORT}`))
